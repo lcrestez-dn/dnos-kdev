de91255ade0dddd1449a40b2da7eb75154a0ae59
Leonard Crestez <cdleonard@gmail.com>
2022-07-04 13:25:14 +0300

    test_main.py: Initial basic pytest
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

---
+++ test_main.py
@@ -0,0 +1,27 @@
+import subprocess
+from pathlib import Path
+
+import pytest
+
+
+def get_main_path():
+    return Path(__file__).parent / "dnos-kdev"
+
+
+def run(argv, **kw):
+    return subprocess.run(
+        [get_main_path()] + argv, text=True, capture_output=True, **kw
+    )
+
+
+def test_run_help():
+    proc = run(["--help"])
+    assert "Kernel tool for dnos" in proc.stderr
+    assert proc.returncode != 0
+
+
+def test_run_dry():
+    proc = run(["--dry-run", "kpatch-build"])
+    assert "DRY: docker build" in proc.stderr
+    assert "DRY: docker run" in proc.stderr
+    assert proc.returncode == 0
